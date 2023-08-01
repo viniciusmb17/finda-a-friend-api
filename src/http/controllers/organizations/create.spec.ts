ad3eb63a1ef71d19de3533fe441914cae5b23e6a
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-08-01 19:46:19 -0300

    feat: controller create and e2e test

--- src/http/controllers/organizations/create.spec.ts
+++ src/http/controllers/organizations/create.spec.ts
@@ -1,5 +1,27 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-  expect(2 + 2).toBe(4)
+describe('Create (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create an organization', async () => {
+    const response = await request(app.server).post('/organizations').send({
+      name: 'John Doe',
+      password: '123456',
+      confirmPassword: '123456',
+      email: 'johndoe@example.com',
+      address: 'Rua teste',
+      cep: '37950000',
+      whatsappNumber: '99 99999-9999',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
--- src/http/controllers/organizations/routes.ts
+++ src/http/controllers/organizations/routes.ts
@@ -2,5 +2,5 @@ import { FastifyInstance } from 'fastify'
 import { create } from './create'
 
 export async function organizationsRoutes(app: FastifyInstance) {
-  app.post('/org', create)
+  app.post('/organizations', create)
 }
